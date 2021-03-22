2749241336be76a7dbab7fbbd035461906920569
David Bonnici <davidbonnici1984@gmail.com>
2021-03-22 23:19:43 +0100

    Added API functionality to Link Hardware Board to Hardware Input Selector - Completed

---
+++ src/OpenA3XX.Core/Dtos/LinkExtenderBitToHardwareInputSelectorDto.cs
@@ -0,0 +1,14 @@
+﻿namespace OpenA3XX.Core.Dtos
+{
+    public class LinkExtenderBitToHardwareInputSelectorDto
+    {
+        public int HardwareInputSelectorId { get; set; }
+        
+        public int HardwareBoardId { get; set; }
+        
+        public int HardwareExtenderBusId { get; set; }
+        
+        public int HardwareExtenderBusBitId { get; set; }
+        
+    }
+}
