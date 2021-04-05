a833127f2d764df4f529813ed9c52ae1627e3481
David Bonnici <davidbonnici1984@gmail.com>
2021-04-05 21:47:33 +0200

    Refactoring Background Service

--- src/OpenA3XX.Peripheral.WebApi/Hubs/ConsumeRabbitMqHostedService.cs
+++ src/OpenA3XX.Peripheral.WebApi/Hubs/ConsumeRabbitMqHostedService.cs
@@ -1,9 +1,14 @@
-﻿using System.Text;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.SignalR;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using OpenA3XX.Core.Repositories;
 using RabbitMQ.Client;
 using RabbitMQ.Client.Events;
 
@@ -13,19 +18,40 @@ namespace OpenA3XX.Peripheral.WebApi.Hubs
 {  
     private readonly ILogger _logger;
     private readonly IHubContext<ChatHub> _hubContext;
+    private readonly IServiceProvider _serviceProvider;
     private IConnection _connection;  
-    private IModel _channel;  
+    private IModel _channel;
+    private Dictionary<string, string> _systemConfiguration;
   
-    public ConsumeRabbitMqHostedService(ILogger<ConsumeRabbitMqHostedService> logger, IHubContext<ChatHub> hubContext)
+    public ConsumeRabbitMqHostedService(ILogger<ConsumeRabbitMqHostedService> logger, IHubContext<ChatHub> hubContext, IServiceProvider serviceProvider)
     {
         _logger = logger;
         _hubContext = hubContext;
+        _serviceProvider = serviceProvider;
         InitRabbitMq();
     }  
   
-    private void InitRabbitMq()  
-    {  
-        var factory = new ConnectionFactory { HostName = "192.168.50.22", UserName = "opena3xx", Password = "opena3xx", VirtualHost = "/"};  
+    private void InitRabbitMq()
+    {
+        using var scope = _serviceProvider.CreateScope();
+        
+        var systemConfigurationRepository = scope.ServiceProvider.GetRequiredService<ISystemConfigurationRepository>();
+            
+        var configuration = systemConfigurationRepository.GetAllConfiguration()
+            .Where(c => c.Key.StartsWith("opena3xx-amqp-"))
+            .ToDictionary(
+                k=>k.Key, 
+                v=>v.Value
+            );
+
+        _systemConfiguration = configuration;
+        
+        var factory = new ConnectionFactory { 
+            HostName = configuration["opena3xx-amqp-host"], 
+            UserName = configuration["opena3xx-amqp-username"], 
+            Password = configuration["opena3xx-amqp-password"], 
+            VirtualHost = configuration["opena3xx-amqp-vhost"]
+        };  
   
         // create connection  
         _connection = factory.CreateConnection();  
@@ -33,10 +59,10 @@ namespace OpenA3XX.Peripheral.WebApi.Hubs
         // create channel  
         _channel = _connection.CreateModel();  
   
-        _channel.QueueDeclare("hardware_events", false, false, false, null);
+        _channel.QueueDeclare(configuration["opena3xx-amqp-hardware-input-selector-events-queue-name"], false, false, false, null);
         _channel.BasicQos(0, 1, false);  
   
-        _connection.ConnectionShutdown += RabbitMQ_ConnectionShutdown;  
+        _connection.ConnectionShutdown += RabbitMQ_ConnectionShutdown;
     }  
   
     protected override Task ExecuteAsync(CancellationToken stoppingToken)  
@@ -47,7 +73,7 @@ namespace OpenA3XX.Peripheral.WebApi.Hubs
         consumer.Received += (ch, ea) =>  
         {  
             // received message  
-            var content = System.Text.Encoding.UTF8.GetString(ea.Body.ToArray());  
+            var content = Encoding.UTF8.GetString(ea.Body.ToArray());  
   
             // handle the received message  
             HandleMessage(content);  
@@ -59,21 +85,20 @@ namespace OpenA3XX.Peripheral.WebApi.Hubs
         consumer.Unregistered += OnConsumerUnregistered;  
         consumer.ConsumerCancelled += OnConsumerConsumerCancelled;  
   
-        _channel.BasicConsume("hardware_events", false, consumer);  
+        _channel.BasicConsume(_systemConfiguration["opena3xx-amqp-hardware-input-selector-events-queue-name"], false, consumer);  
         return Task.CompletedTask;  
     }  
   
     private void HandleMessage(string content)  
     {  
-        // we just print this message   
-        _hubContext.Clients.All.SendAsync("messageReceivedFromRabbitMq", content);
+        _hubContext.Clients.All.SendAsync(_systemConfiguration["opena3xx-amqp-signalr-hardware-events-proxy-name"], content);
     }  
       
-    private void OnConsumerConsumerCancelled(object sender, ConsumerEventArgs e)  {  }  
-    private void OnConsumerUnregistered(object sender, ConsumerEventArgs e) {  }  
-    private void OnConsumerRegistered(object sender, ConsumerEventArgs e) {  }  
-    private void OnConsumerShutdown(object sender, ShutdownEventArgs e) {  }  
-    private void RabbitMQ_ConnectionShutdown(object sender, ShutdownEventArgs e)  {  }  
+    private static void OnConsumerConsumerCancelled(object sender, ConsumerEventArgs e)  {  }  
+    private static void OnConsumerUnregistered(object sender, ConsumerEventArgs e) {  }  
+    private static void OnConsumerRegistered(object sender, ConsumerEventArgs e) {  }  
+    private static void OnConsumerShutdown(object sender, ShutdownEventArgs e) {  }  
+    private static void RabbitMQ_ConnectionShutdown(object sender, ShutdownEventArgs e)  {  }  
   
     public override void Dispose()  
     {  
